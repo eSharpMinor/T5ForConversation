b49c28ba062d92a38872a192416e7dd1733f2096
eSharpMinor <pokhrel230@gmail.com>
2023-12-04 22:22:35 +0545

    Added bash scripts...

---
+++ train_base.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 train_base.py --data_dir="./Conversation.csv" --output_dir="./output_base/" --num_train_epochs=10
---
+++ train_small.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 train_small.py --data_dir="./Conversation.csv" --output_dir="./output_small/" --num_train_epochs=10
